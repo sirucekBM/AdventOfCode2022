c3a073ca8a1580acfbe469b01121267f65d07c85
sirucekBM <bmartins@seznam.cz>
2022-12-12 19:30:35 +0100

    aktualizace

--- 12a.py
+++ 12a.py
@@ -1,3 +1,18 @@
+import collections
+
 with open("data\\12data.txt") as data:
     listX = data.readlines()
 
+#tady si jen pro info načtu do slovníku písmena-----------
+slovnikBodu={}
+for r in listX:
+    for p in r:
+        if(len(p.strip())<1):
+            continue
+        if p in slovnikBodu:           
+            slovnikBodu[p]=slovnikBodu[p] + 1
+        else:
+            slovnikBodu[p]=1
+od = collections.OrderedDict(sorted(slovnikBodu.items()))
+print('k')
+#---------------------------------------------------------
